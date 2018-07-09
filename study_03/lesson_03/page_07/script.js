5d159f062be93abb67f8d7785f06cce1c384651f
Edilson Alves <email@edilsonalves.com>
2018-07-09 19:00:55 -0300

    Lesson 03 completed (Study 03)

---
+++ study_03/lesson_03/page_07/script.js
@@ -0,0 +1,3 @@
+var user = {'name': 'Ken the Ninja', 'age': 14};
+console.log(user.name);
+console.log(user['age']);
---
+++ study_03/lesson_03/page_08/script.js
@@ -0,0 +1,5 @@
+var user = {'name': 'Ken the Ninja', 'age': 14};
+user.name = 'Master White';
+console.log(user.name);
+user.age = 1000;
+console.log(user.age);
